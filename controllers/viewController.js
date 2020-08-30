305b856783cfac72ee3422a2a3614bd2a29ad99d
Lã Đắc Hiếu <hrcp.hieu@gmail.com>
2020-08-31 00:18:41 +0700

    hieu commit 00:18 31/08

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,7 @@
 // const catchAsync = require('./../utils/catchAsync');
 
 exports.showHomePage = (req, res, next) => {
-  res.render('index');
-};
+  res.render('index', {page : "home"})}
+
+exports.showHocvienPage = (req, res, next) => {
+    res.render('index', {page : "hocvien"})}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,5 +4,6 @@ const viewController = require('./../controllers/viewController');
 const router = express.Router();
 
 router.route('/').get(viewController.showHomePage);
+router.route('/hocvien').get(viewController.showHocvienPage);
 
 module.exports = router;
