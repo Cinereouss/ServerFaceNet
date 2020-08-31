a4043312220078af973ecbb58767a61c14b651c4
Lã Đắc Hiếu <hrcp.hieu@gmail.com>
2020-08-31 23:24:54 +0700

    [UPDATE] Cập nhật thêm các views

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,3 +5,12 @@ exports.showHomePage = (req, res, next) => {
 
 exports.showHocvienPage = (req, res, next) => {
     res.render('index', {page : "hocvien"})}
+
+exports.showLichhenPage = (req, res, next) => {
+    res.render('index', {page : "lichhen"})}
+
+exports.showRPTLDPage = (req, res, next) => {
+    res.render('index', {page : "tyledo"})}
+
+exports.showUserPage = (req, res, next) => {
+    res.render('index', {page : "user"})}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.route('/').get(viewController.showHomePage);
 router.route('/hocvien').get(viewController.showHocvienPage);
+router.route('/lichhen').get(viewController.showLichhenPage);
+router.route('/rp-tiledo').get(viewController.showRPTLDPage);
+router.route('/user').get(viewController.showUserPage);
 
 module.exports = router;
