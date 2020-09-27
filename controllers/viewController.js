106755f402508cceb5064daef28581414f472497
Lã Đắc Hiếu <hrcp.hieu@gmail.com>
2020-09-27 23:13:20 +0700

    [IMP] Theme of app

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -6,35 +6,35 @@ exports.showHomePage = (req, res, next) => {
     res.render('index', {page: "home"})
 }
 
-exports.showHocvienPage = (req, res, next) => {
-    res.render('index', {page: "hocvien"})
-}
-
-exports.showLichhenPage = (req, res, next) => {
-    res.render('index', {page: 'lichhen'});
-}
-
-exports.showRPTLDPage = (req, res, next) => {
-    res.render('index', {page: "tyledo"})
-}
-
-exports.showUserPage = (req, res, next) => {
-    res.render('index', {page: "user"})
-}
-
-exports.showDiemdanhPage = (req, res, next) => {
-    Attendance.find({})
-        .populate('idLearner')
-        .limit(12)
-        .sort({checkInAt: 'desc'})
-        .select({})
-        .exec((err, attend) => {
-        if(err){
-            res.json({
-                erron : false
-            })
-        }else{
-            res.render("index", {page : "diemdanh", data : attend})
-        }
-    })
-}
+// exports.showHocvienPage = (req, res, next) => {
+//     res.render('index', {page: "hocvien"})
+// }
+//
+// exports.showLichhenPage = (req, res, next) => {
+//     res.render('index', {page: 'lichhen'});
+// }
+//
+// exports.showRPTLDPage = (req, res, next) => {
+//     res.render('index', {page: "tyledo"})
+// }
+//
+// exports.showUserPage = (req, res, next) => {
+//     res.render('index', {page: "user"})
+// }
+//
+// exports.showDiemdanhPage = (req, res, next) => {
+//     Attendance.find({})
+//         .populate('idLearner')
+//         .limit(12)
+//         .sort({checkInAt: 'desc'})
+//         .select({})
+//         .exec((err, attend) => {
+//         if(err){
+//             res.json({
+//                 erron : false
+//             })
+//         }else{
+//             res.render("index", {page : "diemdanh", data : attend})
+//         }
+//     })
+// }
--- public/js/index.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-alert('Hello EJS');
