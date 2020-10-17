e54eec7c69e3bbff743371d4a9cb3a793d3353c4
Hieu LD <hrcp.hieu@gmail.com>
2020-10-17 08:01:25 +0700

    [FINiSH] profile user and sign in online.
    Form đăng ký online
    Giao diện hồ sơ người dùng

--- controllers/backendController.js
+++ controllers/backendController.js
@@ -1,5 +1,9 @@
 const catchAsync = require('./../utils/catchAsync');
 const HocVien = require('../models/hocvienModel')
+const Log = require('../models/logModel')
+const Lop = require('../models/lopModel')
+
+const mongoose = require('mongoose')
 
 exports.showHomePage = async (req, res, next) => {
     const pending = await HocVien.find({pending : true})
@@ -14,5 +18,15 @@ exports.showDondkoPage = async (req, res, next) => {
 exports.showProfilePage = async (req, res, next) => {
     const hocvien = await HocVien.findById(req.params.id)
     const pending = await HocVien.find({pending : true})
-    res.render('backend', {page: "B_profile", hocvien: hocvien, pending: pending})
+    if(hocvien.embedding != null){
+        const log = await Log.find({
+            idHocvien : mongoose.Schema.Types.ObjectId(hocvien._id)
+        });
+        const lop = await Lop.findById(hocvien.idLop)
+        console.log(log)
+        res.render('backend', {page: "B_profile", hocvien: hocvien, pending: pending, log: log, lop: lop});
+    }else{
+        res.render('backend', {page: "B_profile", hocvien: hocvien, pending: pending})
+    }
+
 }
