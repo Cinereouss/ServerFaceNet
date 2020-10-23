156f4467e3bfe9d2c337494400fcf54cf86c7cac
unknown <ngapham.vmu@gmail.com>
2020-10-23 19:53:04 +0700

    nga commit

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -24,3 +24,99 @@ exports.register = (req, res, next) => {
 exports.showGioiThieuPage = (req, res, next) => {
     res.render('frontend', {page : "F_gioithieu"})
 }
+
+exports.showKhoaHocPage = (req, res, next) => {
+    res.render('frontend', {page : "F_khoahoc"})
+}
+
+exports.showTaiLieuPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tailieu"})
+}
+
+exports.showTinTucPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tintuc"})
+}
+
+exports.showHocPhiPage = (req, res, next) => {
+    res.render('frontend', {page : "F_hocphi"})
+}
+
+exports.showLienHePage = (req, res, next) => {
+    res.render('frontend', {page : "F_lienhe"})
+}
+
+exports.showKhoaHocB2Page = (req, res, next) => {
+    res.render('frontend', {page : "F_khoahocB2"})
+}
+
+exports.showXeMayMoToPage = (req, res, next) => {
+    res.render('frontend', {page : "F_xemaymoto"})
+}
+
+exports.showHocKhacPage = (req, res, next) => {
+    res.render('frontend', {page : "F_hockhac"})
+}
+
+exports.showB2TieuChuanPage = (req, res, next) => {
+    res.render('frontend', {page : "F_B2tieuchuan"})
+}
+
+exports.showB2DacBietPage = (req, res, next) => {
+    res.render('frontend', {page : "F_B2dacbiet"})
+}
+
+exports.showB2ChiThiPage = (req, res, next) => {
+    res.render('frontend', {page : "F_B2chithi"})
+}
+
+exports.showXeSoTuDongB11Page = (req, res, next) => {
+    res.render('frontend', {page : "F_xesotudongB11"})
+}
+
+exports.showLaiXeA1Page = (req, res, next) => {
+    res.render('frontend', {page : "F_laixeA1"})
+}
+
+exports.showMoToA2Page = (req, res, next) => {
+    res.render('frontend', {page : "F_motoA2"})
+}
+
+exports.showLaiXeA3Page = (req, res, next) => {
+    res.render('frontend', {page : "F_laixeA3"})
+}
+
+exports.showXeTaiCPage = (req, res, next) => {
+    res.render('frontend', {page : "F_xetaiC"})
+}
+
+exports.showThueXePage = (req, res, next) => {
+    res.render('frontend', {page : "F_thuexe"})
+}
+
+exports.showNangHangPage = (req, res, next) => {
+    res.render('frontend', {page : "F_nanghang"})
+}
+
+exports.showTaiLieuLyThuyetPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tailieulythuyet"})
+}
+
+exports.showTaiLieuThucHanhPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tailieuthuchanh"})
+}
+
+exports.showTinTucoToPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tintucoto"})
+}
+
+exports.showTinTucTrungTamPage = (req, res, next) => {
+    res.render('frontend', {page : "F_tintuctrungtam"})
+}
+
+exports.showThuGianPage = (req, res, next) => {
+    res.render('frontend', {page : "F_thugian"})
+}
+
+exports.showMeNuHocPhiPage = (req, res, next) => {
+    res.render('frontend', {page : "F_menuhocphi"})
+}
